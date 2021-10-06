917ca1faba405b45093b03fc801815f514b15aec
AniElaina <ani.elaina.carr@gmail.com>
2021-10-06 14:59:22 -0400

    broke AF

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
 
 const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
@@ -10,14 +11,10 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
-async function startServer() {
-  await server.start();
-  server.applyMiddleware({ app });
-}
-
-startServer();
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
